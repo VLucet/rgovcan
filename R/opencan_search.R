c6e1d4ebdbac6be32a4399b63fb0a19a51988dd5
VLucet <valentin.lucet@gmail.com>
2019-05-11 21:36:28 -0400

    change serach function

--- R/opencan_search.R
+++ R/opencan_search.R
@@ -3,12 +3,17 @@
 #' @param keywords A set of keywords to query
 #' @param domain Domain name for the OpenCan API being querried
 #' @param format Whether the function should return a formatted output
+#' @param row_size The number of row to return from the json query
+#'
+#' @return If format is TRUE, will return a formatted dataframe, else it will return
+#' the unformatted json R object from jsonlite::fromJSON
 #'
 #' @export
 
 opencan_search <- function(keywords,
                            domain = "https://open.canada.ca/data/en/api/3/",
-                           format = TRUE) {
+                           format = TRUE,
+                           row_size = 10000000) {
 
   keywords_collated <- paste0(keywords,"+")
   search_url <- paste0(domain, keywords_collated)
@@ -16,9 +21,9 @@ opencan_search <- function(keywords,
   output_json <- purrr::map(search_url, jsonlite::fromJSON)
 
   if (format == TRUE) {
-
-    NULL
-
+    output_json
+  } else {
+    query_out <- output_json
   }
-
+  query_out
 }
