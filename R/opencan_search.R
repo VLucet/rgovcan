d1922ebbfb1ee241427d711ad50e888bc19f0c7a
VLucet <valentin.lucet@gmail.com>
2019-05-11 22:17:01 -0400

    serach function now operational

--- R/opencan_search.R
+++ R/opencan_search.R
@@ -3,7 +3,7 @@
 #' @param keywords A set of keywords to query
 #' @param domain Domain name for the OpenCan API being querried
 #' @param format Whether the function should return a formatted output
-#' @param row_size The number of row to return from the json query
+#' @param row_size The number of row in the JSON object to return from the solr query
 #'
 #' @return If format is TRUE, will return a formatted dataframe, else it will return
 #' the unformatted json R object from jsonlite::fromJSON
@@ -13,15 +13,33 @@
 opencan_search <- function(keywords,
                            domain = "https://open.canada.ca/data/en/api/3/",
                            format = TRUE,
-                           row_size = 10000000) {
+                           row_size = 10000) {
 
+  # Collates all keywords
   keywords_collated <- paste0(keywords,"+")
-  search_url <- paste0(domain, keywords_collated)
+  keywords_collated <- substr(keywords_collated,1, nchar(keywords_collated)-1)
 
-  output_json <- purrr::map(search_url, jsonlite::fromJSON)
+  # Construct the search url, with the package_search API function
+  search_url <- paste0(domain,
+                       "action/package_search?q=", keywords_collated,
+                       "&rows=", row_size)
+
+  # Extract query
+  output_json <- purrr::map(search_url, jsonlite::fromJSON)[[1]]
+
+  # Tell the user how many records were found
+  message("OpenCan Query: ", output_json$result$count,
+          " records found for keywords: ",keywords)
 
   if (format == TRUE) {
-    output_json
+
+    # Extract dataframe
+    query_results <- output_json$result$results
+    #
+    # More formtting of the results needed, for now outputs dataframe
+    #
+    query_out <- query_results
+
   } else {
     query_out <- output_json
   }
