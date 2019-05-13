3410ee139e7b95992762cda9c965ece2d6098fea
VLucet <valentin.lucet@gmail.com>
2019-05-13 13:57:37 -0400

    update the hex

--- inst/hex_plot.R
+++ inst/hex_plot.R
@@ -4,9 +4,9 @@ library(hexSticker)
 
 # png picture under CC http://www.pngall.com/canada-leaf-png/download/14302
 
-sticker("canada.png",
+sticker("inst/canada.png",
         p_color = "#CB4335", p_y = 1.45,
-        package="ropencan", p_size=8, s_x=1, s_y=.75, s_width=.5,
+        package="rgovcan", p_size=8, s_x=1, s_y=.75, s_width=.5,
         h_fill="#D6EAF8", h_color="#2E4053",
         s_height = 0.5,
-        filename="inst/ropencan_hex.png", dpi=1000)
+        filename="inst/rgovcan_hex.png", dpi=1000)
