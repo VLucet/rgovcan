fa9a26cab4b8ff3a719c9f421a480a420463f3af
Valentin <valentin.lucet@gmail.com>
2021-03-10 09:21:02 -0500

    fully cover get_record

--- tests/testthat/helper-tests.R
+++ tests/testthat/helper-tests.R
@@ -4,6 +4,10 @@ library(rgovcan)
 # PID and Pkg
 pid <- "b7ca71fa-6265-46e7-a73c-344ded9212b0"
 pkg <- govcan_get_record(pid)
+pkg_list_format <- govcan_get_record(pid, format_resources = TRUE)
+pkg_list_res <- govcan_get_record(pid, only_resources = TRUE)
+pkg_list_res_format <- govcan_get_record(pid, format_resources = TRUE,
+                                         only_resources = TRUE)
 
 # Search
 search_default <- govcan_search("dfo", 10)
--- tests/testthat/test-get_record.R
+++ tests/testthat/test-get_record.R
@@ -1,5 +1,18 @@
 
 test_that("test format", {
+
   expect_equal(class(pkg), "ckan_package")
   expect_equal(pkg$id, pid)
+  expect_equal(class(), "list")
+
+  expect_equal(class(pkg_list_format), "list")
+  expect_equal(pkg_list_format$id, pid)
+  expect_equal(class(pkg_list_format$resources)[1], "tbl_df")
+
+  expect_equal(class(pkg_list_res), "list")
+  expect_equal(length(pkg_list_res), 6)
+  expect_equal(length(pkg_list_res[[1]]), 22)
+
+  expect_equal(class(pkg_list_res_format)[1], "tbl_df")
+  expect_equal(dim(pkg_list_res_format), c(6, 22))
 })
