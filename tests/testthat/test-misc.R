cda50ac50d859a01842c8d5b0fe221ff9f626349
Valentin <valentin.lucet@gmail.com>
2021-03-10 08:35:43 -0500

    add tests for misc

---
+++ tests/testthat/test-misc.R
@@ -0,0 +1,11 @@
+
+test_that("Misc functions work as expected", {
+
+  expect_message(rgovcan:::msgError("Error"), "Error")
+  expect_message(rgovcan:::msgSuccess("Success"), "Success")
+  expect_message(rgovcan:::msgWarning("Warning"), "Warning")
+
+  expect_true(is.na(rgovcan:::null_to_na(NULL)))
+  expect_identical(rgovcan:::null_to_na(list(NULL, NULL)), list(NA, NA))
+
+})
