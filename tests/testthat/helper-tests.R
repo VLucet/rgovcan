0c7c257fd189bede082fa91f528eb696a6a5a117
Valentin <valentin.lucet@gmail.com>
2021-03-10 08:46:37 -0500

    add checks for ressources

--- tests/testthat/helper-tests.R
+++ tests/testthat/helper-tests.R
@@ -15,3 +15,4 @@ search_list_format <- govcan_search("dfo", 10, format_results = TRUE,
 
 # Resources
 id_resources <- govcan_get_resources(search_default)
+id_resources_character <- govcan_get_resources("b7ca71fa-6265-46e7-a73c-344ded9212b0")
---
+++ tests/testthat/test-get_resources.R
@@ -0,0 +1,11 @@
+
+test_that("getting resources work", {
+
+  expect_equal(class(id_resources), "list")
+  expect_equal(length(id_resources), 10)
+  expect_equal(class(id_resources[[1]]), "ckan_resource_stack")
+
+  expect_equal(class(id_resources_character), "ckan_resource_stack")
+  expect_equal(length(id_resources_character), 6)
+
+})
